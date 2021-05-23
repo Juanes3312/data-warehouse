6d65d54b06e275cb50800c1e75cec20b83802eb2
Juan Esteban Jaramillo <juanesteban0607@live.com>
2021-05-23 12:36:32 -0500

    terminando el region-ciudad

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -29,7 +29,7 @@ server.delete("/paises/:id", (req,res) =>{
   //console.log(req.params.id, 'soy el id')
   //console.log(id)
   sequelize
-    .query("DELETE * FROM `paises` WHERE `id` = ? ",{
+    .query("DELETE  FROM `paises` WHERE `id` = ? ",{
       replacements : [id],
       type: sequelize.QueryTypes.DELETE
     })
@@ -37,6 +37,89 @@ server.delete("/paises/:id", (req,res) =>{
       res.json(results);
     });
 })
+
+server.delete("/ciudades/:id", (req,res) =>{
+  let {id} = req.params
+  sequelize
+    .query("DELETE  FROM `ciudades` WHERE `id` = ? ",{
+      replacements : [id],
+      type: sequelize.QueryTypes.DELETE
+    })
+    .then(results => {
+      res.json(results);
+    });
+})
+
+server.put("/paises/:id", async function(req,res){
+    const a = req.params.id;
+    const {name} = req.body
+    console.log(name, "soy nombre")
+    const pais = await traerPais(a);
+    if(pais[0]){
+        sequelize.query("UPDATE `paises` SET `nombre` = ? WHERE `paises`.`id` = ?",
+            {
+                replacements:[
+                    name,
+                    a
+                ],
+                type: sequelize.QueryTypes.UPDATE
+            }
+        )
+        .then(()=>{
+            res.status(200).json({
+                "status" : "ok",
+                "mensaje": "el pais ha sido modificado con exito"
+            })
+        })
+    }else{
+        res.status(400).json({
+            "mensaje":"No existe un pais con ese id"
+        });
+    }
+});
+
+server.post('/ciudades',(req,res) =>{
+  const {pais_id,nombre} = req.body;
+  sequelize.query("INSERT INTO ciudades (pais_id, nombre) VALUE(?,?)",{
+      replacements:[pais_id,nombre],
+      type:sequelize.QueryTypes.INSERT
+  })
+  .then(()=>{
+      res.status(200).json({
+          mensaje: "todo correcto mi pana"
+      })
+  })
+})
+
+
+server.put("/ciudades/:id", async function(req,res){
+  const a = req.params.id;
+  const {name} = req.body
+  console.log(name, "soy nombre")
+  const pais = await traerCiudad(a);
+  if(pais[0]){
+      sequelize.query("UPDATE `ciudades` SET `nombre` = ? WHERE `paises`.`id` = ?",
+          {
+              replacements:[
+                  name,
+                  a
+              ],
+              type: sequelize.QueryTypes.UPDATE
+          }
+      )
+      .then(()=>{
+          res.status(200).json({
+              "status" : "ok",
+              "mensaje": "el pais ha sido modificado con exito"
+          })
+      })
+  }else{
+      res.status(400).json({
+          "mensaje":"No existe un pais con ese id"
+      });
+  }
+});
+
 
 //guardar region nueva
 server.post('/regiones',(req,res) =>{
@@ -51,6 +134,17 @@ server.post('/regiones',(req,res) =>{
       })
   })
 })
+
+async function traerPais(id){
+    const res = await sequelize.query('SELECT * FROM paises WHERE paises.id = ?',{replacements:[id],type: sequelize.QueryTypes.SELECT});
+    return res;
+}
+
+async function traerCiudad(id){
+  const res = await sequelize.query('SELECT * FROM ciudades WHERE paises.id = ?',{replacements:[id],type: sequelize.QueryTypes.SELECT});
+  return res;
+}
+
 
 
 //extraer todas las regiones
